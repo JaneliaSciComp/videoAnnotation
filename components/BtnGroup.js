258d83f2bafbd31272050f345d5b29961a839904
Xi Peng <xpengjanelia@gmail.com>
2023-09-08 18:44:58 -0400

    minor layout change

--- components/BtnGroup.js
+++ components/BtnGroup.js
@@ -41,8 +41,10 @@ export default function BtnGroup(props) {
     }
 
     return (
-        <Fragment>
+        // <Fragment>
+        <div className='px-0'>
             {renderBtns()}
-        </Fragment>
+        </div>
+        // </Fragment>
     )
 }
