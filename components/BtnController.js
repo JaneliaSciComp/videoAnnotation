041d2466b2ff8b6447383346cba85043ffc72fe4
Dabenniu13 <19064166@qq.com>
2023-09-13 12:06:52 -0400

    implemented btnController and part of btnGroupController

--- components/BtnController.js
+++ components/BtnController.js
@@ -1,28 +1,31 @@
 import React, {useState, useEffect, useRef, Fragment} from 'react';
 import styles from '../styles/Controller.module.css';
-import { Select, Button, Input, ColorPicker } from 'antd';
-import { DownOutlined, DeleteOutlined} from '@ant-design/icons';
+import { Select, Button, Input, ColorPicker, Space } from 'antd';
+import { DeleteOutlined} from '@ant-design/icons';
 // import {Button} from 'react-bootstrap';
 
 export default function BtnController(props) {
     /*
         To an annotating btn.
         Props: 
-            // key: integer. Required. To distinguish from other btnController
+            index: integer. Required when has btnGroupController as parent. To distinguish from other btnController
+            // data: object. {key: int, groupType: str, btnType: str, label: str, color: str}
             groupType: 'category'/'shape'. Optional. When specified, the btnType list will be generated accordingly; otherwise, use general list.
             btnType: 'bbox'. Optional. When specified, the default value of the btnType select will be set.
             color: 'red'. Optional.
+            label: 'mouse'. Optional.
             typeSelectPlaceHolder: 'Btn type'
             labelPlaceHolder: 'mouse'
             disableTypeSelect: boolean. False by default, true when specified. Whether to disable type selcet.
             disableLabelInput: boolean. False by default, true when specified. Whether to disable label input.
             disableColorPicker: boolean. False by default, true when specified. Whether to disable color picker.
-            disableDelete: boolean. False by default, true when specified. Whether to include the delete btn.
-            onTypeChange: function
-            onLabelChange: function
-            onColorChange: function
-            deleteBtnClickHander: function
+            enableDelete: boolean. False by default, true when specified. Whether to include the delete btn.
+            onBtnTypeChange: Callback when btn type changes
+            onLabelEnter: Callback when use press enter in label input
+            onColorChange: Callback when colorPicker changes
+            onDelete: Callback when delete btn clicked
     */
+    console.log('btnController render', props.index);
 
     const btnOptions = {
         category: [
@@ -42,49 +45,120 @@ export default function BtnController(props) {
     }
 
 
-    function labelChangeHandler(newValue) {
-        console.log('label', newValue);
+    function onBtnTypeChange(value, opt) {
+        console.log('BtnType', value, opt);
+        // no need to use if (props.onLabelChange), because the developer has to use props to set up the callback
+        const target = {
+            index: props.index,
+            value: value,
+            label: opt.label
+        };
+        props.onBtnTypeChange(target);
     }
 
 
-    function colorChangeHandler(newValue) {
-        console.log('color', newValue);
+    function onLabelChange(e) {
+        // console.log('label', e);
+        // no need to use if (props.onLabelChange), because the developer has to use props to set up the callback
+        const target = {
+            index: props.index,
+            value: e.target.value,
+        };
+        props.onLabelChange(target);
+    }
+
+
+    function onColorChange(value) {
+        // console.log('color', value.metaColor.originalInput); //metaColor: {originalInput: '#000000A6', r: float, g:float, b:float, a:float, ...}
+        const target = {
+            index: props.index,
+            value: value.metaColor.originalInput,
+        };
+        props.onColorChange(target);
+    }
+
+
+    function onDelete() {
+        const target = {
+            index: props.index,
+        };
+        props.onDelete(target);
     }
 
 
     return (
-        <div className='px-0'>
+        <div className='d-inline-flex'>
+            <Space.Compact block className='px-0'>
                 <Select className={styles.btnSelect}
                     value={props.btnType}
-                    onChange={props.onTypeChange}
+                    onChange={onBtnTypeChange}
                     options={props.groupType?btnOptions[props.groupType]:btnOptions.general}
                     placeholder={props.typeSelectPlaceHolder}
                     disabled={props.disableTypeSelect?props.disableTypeSelect:false}
                     />
                 <Input className={styles.labelText}
-                    addonBefore="Label"
-                    onChange={props.onLabelChange}
+                    // addonBefore="Label"
+                    allowClear
+                    value={props.label}
+                    // onPressEnter={onLabelEnter}
+                    onChange={onLabelChange}
                     placeholder={props.labelPlaceHolder}
                     disabled = {props.disableLabelInput?props.disableLabelInput:false}
                     />
                 <ColorPicker className={styles.colorPicker}
                     // className={videoStyles.playFpsInput} 
                     value={props.color}
-                    onChange={props.onColorChange}
+                    onChange={onColorChange}
                     disable = {props.disableColorPicker?props.disableColorPicker:false}
                     // size="small"
+                    presets={[
+                        { label: 'Recommended',
+                          colors: [
+                            '#000000',
+                            '#000000E0',
+                            '#000000A6',
+                            '#00000073',
+                            '#00000040',
+                            '#00000026',
+                            '#0000001A',
+                            '#00000012',
+                            '#0000000A',
+                            '#00000005',
+                            '#F5222D',
+                            '#FA8C16',
+                            '#FADB14',
+                            '#8BBB11',
+                            '#52C41A',
+                            '#13A8A8',
+                            '#1677FF',
+                            '#2F54EB',
+                            '#722ED1',
+                            '#EB2F96',
+                            '#F5222D4D',
+                            '#FA8C164D',
+                            '#FADB144D',
+                            '#8BBB114D',
+                            '#52C41A4D',
+                            '#13A8A84D',
+                            '#1677FF4D',
+                            '#2F54EB4D',
+                            '#722ED14D',
+                            '#EB2F964D',
+                          ],
+                        },
+                    ]}
                     />
-                {props.disableDelete ?
-                    null : 
-                    <Button className={styles.deleteBtn} 
-                    // shape='circle'
-                    type='text'
-                    icon={<DeleteOutlined />} 
-                    onClick={props.deleteBtnClickHander}
-                    // size='small'
-                    />
-                }
-
-            </div>
+            </Space.Compact>
+            {props.enableDelete ?
+                <Button className={styles.deleteBtn} 
+                // shape='circle'
+                type='text'
+                icon={<DeleteOutlined />} 
+                onClick={onDelete}
+                // size='small'
+                />
+                :null
+            }
+        </div>
     )
 }
--- components/BtnGroupController.js
+++ components/BtnGroupController.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect, useRef, Fragment} from 'react';
 import styles from '../styles/Controller.module.css';
 import BtnController from './BtnController';
-import { Select, InputNumber, Button } from 'antd';
+import { Select, InputNumber, Button, Space } from 'antd';
 import { DownOutlined, DeleteOutlined} from '@ant-design/icons';
 // import {Button} from 'react-bootstrap';
 
@@ -10,8 +10,11 @@ const BTNNUM_MAX=50
 export default function BtnGroupController(props) {
     const [groupType, setGroupType] = useState();
     const [btnType, setBtnType] = useState();
-    const [btnNum, setBtnNum] = useState(1);
-    const [children, setChildren] = useState();
+    const [btnNum, setBtnNum] = useState(0);
+    const [children, setChildren] = useState([]);
+    const [childrenData, setChildrenData] = useState([]);
+    const prevBtnNumRef = useRef(0);
+    const prevBtnTypeRef = useRef();
 
     console.log('btnGroupController render');
 
@@ -32,103 +35,185 @@ export default function BtnGroupController(props) {
     }
 
 
-    function groupTypechangeHandler(newValue) {
+    useEffect(() => {
+        // to update callback's scope when childrenData changes
+        // console.log('useEffect called');
+        if (groupType && btnType) {
+            renderChildren();
+        }
+      }, [childrenData]
+    )
+
+
+    function groupTypeChangeHandler(newValue) {
         // console.log(newValue);
         // console.log(btnOptions[newValue]);
         setGroupType(newValue);
         setBtnType(btnOptions[newValue][0]['value']);
     }
 
-    function btnTypeChangeHandler(newValue) {
+    function btnTypeChangeHandler(newValue, opt) {
+        console.log(opt);
         setBtnType(newValue);
     }
 
     function numChangeHandler(newValue) {
         if (typeof newValue === 'number' 
         && Number.isInteger(newValue) 
-        && newValue>=1 
+        && newValue>=0 
         && newValue<=BTNNUM_MAX) {
             setBtnNum(newValue);
         }
     }
 
-    function generateBtnClickHandler() {
-        if (groupType && btnType && btnNum) {
-            const res = renderChildren();
-            setChildren(res);
-        } 
+    function downBtnClickHandler() {
+        if (groupType && btnType) {
+            if (btnNum > prevBtnNumRef.current) {
+                const newData = createChildrenData(prevBtnNumRef.current, btnNum);
+                setChildrenData([...childrenData, ...newData]);
+            } else if (btnNum < prevBtnNumRef.current) {
+                const newData = [...childrenData].slice(0, btnNum);
+                setChildrenData(newData);
+            } else {
+                if (btnType !== prevBtnTypeRef.current) {
+                    const newData = createChildrenData(0, btnNum);
+                    setChildrenData(newData);
+                }
+            }
+            prevBtnNumRef.current = btnNum;
+            prevBtnTypeRef.current = btnType;
+        } else {
+            setChildrenData([]);
+        }
     }
 
-    function deleteBtnClickHandler() {
-
+    function createChildrenData(startIndex, endIndex) {
+        const newData=[];
+        for (let i = startIndex; i < endIndex; i++) {
+            const data = {
+                index: i,
+                groupType: groupType,
+                btnType: btnType,
+            };
+            newData.push(data);
+        }
+        return newData;
     }
 
 
     function renderChildren() {
+        console.log('renderChildren called');
         let res = [];
-        for (let i = 0; i < props.btnNum; i++) {
+        for (let i = 0; i < childrenData.length; i++) {
             res.push(
                 <BtnController 
-                    key={i} 
-                    groupType={groupType}
-                    btnType={btnType}
+                    key={i}
+                    index={i} 
+                    // data={childrenData[i]}
+                    groupType={childrenData[i].groupType}
+                    btnType={childrenData[i].btnType}
+                    color={childrenData[i].color}
+                    label={childrenData[i].label}
                     typeSelectPlaceHolder='Btn type'
-                    labelPlaceHolder='mouse'
+                    labelPlaceHolder="Label: e.g. 'mouse'"
                     disableTypeSelect
-                    // onLabelChange={} 
-                    // onColorChange={} 
-                    // deleteBtnClickHander={}
+                    enableDelete
+                    // onTypeChange={onChildTypeChange}
+                    onLabelChange={onChildLabelChange}
+                    onColorChange={onChildColorChange}
+                    onDelete={onChildDelete}
                     />); 
         }
-        return res;
+        setChildren(res);
+    }
+
+
+    function onDelete() {
+        
+    }
+
+    
+    function onChildLabelChange(target) {
+        // console.log('parent ',target);
+        const data = {...childrenData[target.index]};
+        console.log('label', children[target.index]);
+        data.label = target.value;
+        // console.log(data);
+        const dataArray = [...childrenData];
+        dataArray[target.index] = data;
+        setChildrenData(dataArray);
+    } 
+
+    function onChildColorChange(target) {
+        const data = {...childrenData[target.index]};
+        // console.log('color', childrenData);
+        data.color = target.value;
+        // console.log(data);
+        const dataArray = [...childrenData];
+        dataArray[target.index] = data;
+        setChildrenData(dataArray);
+    }
+
+    function onChildDelete(target) {
+        setBtnNum(btnNum-1);
+        prevBtnNumRef.current = prevBtnNumRef.current>0 ? prevBtnNumRef.current-1 : 0;
+        const newChildrenData = [...childrenData];
+        newChildrenData.splice(target.index, 1);
+        newChildrenData.forEach((item,i) => {item.index=i});
+        setChildrenData(newChildrenData);
     }
 
 
     return (
-        <Fragment>
-            <div className='px-0'>
-                <Select className={styles.groupSelect}
-                    // defaultValue={btnGroupOptions[0].value}
-                    onChange={groupTypechangeHandler}
-                    options={btnGroupOptions}
-                    placeholder='Group type'
-                    />
-                <Select className={styles.btnSelect}
-                    value={btnType}
-                    onChange={btnTypeChangeHandler}
-                    options={btnOptions[groupType]}
-                    placeholder='Btn type'
-                    />
-                <InputNumber className={styles.numInput}
-                    // className={videoStyles.playFpsInput} 
-                    min={1}
-                    max={BTNNUM_MAX}
-                    value={btnNum}
-                    onChange={numChangeHandler}
-                    // size="small"
-                    />
+        <div className='my-1'>
+            <div className='my-2 d-inline-flex'>
+                <Space.Compact block className='px-0'>
+                    <Select className={styles.groupSelect}
+                        // defaultValue={btnGroupOptions[0].value}
+                        onChange={groupTypeChangeHandler}
+                        options={btnGroupOptions}
+                        placeholder='Group type'
+                        />
+                    <Select className={styles.btnSelect}
+                        value={btnType}
+                        onChange={btnTypeChangeHandler}
+                        options={btnOptions[groupType]}
+                        placeholder='Btn type'
+                        />
+                    <InputNumber className={styles.numInput}
+                        // className={videoStyles.playFpsInput} 
+                        min={0}
+                        max={BTNNUM_MAX}
+                        value={btnNum}
+                        onChange={numChangeHandler}
+                        // size="small"
+                        />
+                    
+                </Space.Compact>
                 <Button className={styles.generateBtn} 
                     // shape='circle'
                     type='text'
                     icon={<DownOutlined />}
-                    onClick={generateBtnClickHandler} 
+                    onClick={downBtnClickHandler} 
                     // size='small'
                     />
                 <Button className={styles.deleteBtn} 
                     // shape='circle'
                     type='text'
                     icon={<DeleteOutlined />} 
-                    onClick={deleteBtnClickHandler}
+                    onClick={onDelete}
                     // size='small'
                     />
             </div>
-            {children ?
-                <div>
-                    {children}
-                </div>
-                :null
-            }
-            
-        </Fragment>
+            <div className='ms-3'>
+                {children ?
+                    <Space direction='vertical'>
+                        {children}
+                    </Space>
+                    
+                    : null
+                }
+            </div>
+        </div>
     )
 }
