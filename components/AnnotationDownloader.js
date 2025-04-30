1a07e0e10bc327fa1f9c6cdb45f0e36ba26f41ef
Mary Lay <mrs.mary.lay@gmail.com>
2025-04-30 16:11:46 -0400

    feature: new components

---
+++ components/AnnotationDownloader.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import { Modal, Input, Button } from 'antd';
+
+export default function AnnotationDownloader({annotationDownloadOpen, setAnnotationDownloadOpen}){
+  //const [annotationDownloadOpen, setAnnotationDownloadOpen] = useState(false);
+  const [fileName, setFileName] = useState('annotations');
+
+  function onDownloadBtnClick() { //What if there's no project or video loaded?  
+      setAnnotationDownloadOpen(true);
+    } 
+  
+  useEffect(()=>{
+    if (annotationDownloadOpen) {  
+      setFileName("videoname");
+      console.log("setting videoname");
+    }
+  }, [annotationDownloadOpen]);
+      
+  function onSaveBtnClick(){
+    console.log("onSaveBtnClick");
+    const finalFileName = fileName;
+    console.log('filename: ', fileName)
+    setAnnotationDownloadOpen(false);
+    const jsonAnno = JSON.stringify(notes);  
+    const blobAnno = new Blob([jsonAnno], {type: 'text/plain'});  
+    const a = document.createElement("a");  
+    a.href = URL.createObjectURL(blobAnno);  
+    console.log("AnnotationName:", fileName);
+    a.download = finalFileName + '.json';
+    a.click();
+    URL.revokeObjectURL(a.href);
+  }
+     
+  return( 
+    <Modal 
+      title={'Download notes'}
+      footer={() => null}
+      open={annotationDownloadOpen}
+      onCancel={() => setAnnotationDownloadOpen(false)}
+      >
+      <div className='my-4 d-flex justify-content-center'>
+      <div style={{width: '100%'}}>
+        Please name your file:
+        <Input defaultValue='videoname' onChange={(e)=>setFileName(e.target.value)} value={fileName} addonAfter='.json' onPressEnter={onSaveBtnClick} />
+        <Button onClick={onSaveBtnClick}>Save</Button>
+      </div>
+      </div>
+    </Modal>
+  )
+}
