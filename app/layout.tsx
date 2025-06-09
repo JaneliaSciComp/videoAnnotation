ebec1a22e536f696e5b9712006297f915681b79d
Jody Clements <jody@neomorphic.co.uk>
2025-06-09 15:38:51 -0400

    fix: removes <></> tags.
    These were not needed here.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,16 @@
 "use client";
 
-import '../styles/globals.css';
-import Workspace from '../components/Workspace';
+import "../styles/globals.css";
+import Workspace from "../components/Workspace";
 
-function MyApp({ children,}: Readonly<{children: React.ReactNode;}>) {
-
+function MyApp({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <>
-      <html>
-        <body>
-          <Workspace>
-            {children}
-          </Workspace>
-        </body>
-      </html>
-    </>
-  )
-  
+    <html>
+      <body>
+        <Workspace>{children}</Workspace>
+      </body>
+    </html>
+  );
 }
 
-export default MyApp
+export default MyApp;
